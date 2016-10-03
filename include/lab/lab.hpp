7c054b9879b90f31c995b85669bf3321480b5acb
tim <timothee.napoli@laposte.net>
2016-10-03 12:45:51 +0200

    Add the 'lab' namespace.
    The lab namespace is the root namespace of the project. All submodule x will
    have a path like "lab::x".
    
    Moreover, a namespace must fit in its own directory, using a logical
    hierarchy.

---
+++ include/lab/lab.hpp
@@ -0,0 +1,17 @@
+/**
+ * @module      lab
+ * @brief       Labengine main header file.
+ * @author      Timothée Napoli
+ * @license     WTFPL
+ * @copyright   Timothée Napoli (c) 2016
+ */
+#ifndef _lab_hpp_
+#define _lab_hpp_
+
+#include "lab/util/util.hpp"
+
+namespace lab {
+
+}
+
+#endif
