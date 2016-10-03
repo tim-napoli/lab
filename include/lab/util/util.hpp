e79f1feae28c13ad596856d406f9270ed50ad332
tim <timothee.napoli@laposte.net>
2016-10-03 12:48:05 +0200

    Add the 'lab::util' submodule.
    The lab::util module is intended to store every common utility of every other
    modules, like exceptions, basic functions, loggers, lab values, etc.

---
+++ include/lab/util/util.hpp
@@ -0,0 +1,20 @@
+/**
+ * @module      lab::util
+ * @brief       Labengine util module header file.
+ * @author      Timothée Napoli
+ * @license     WTFPL
+ * @copyright   Timothée Napoli (c) 2016
+ */
+#ifndef _lab_util_hpp_
+#define _lab_util_hpp_
+
+namespace lab {
+
+namespace util {
+
+}
+
+}
+
+#endif
+
