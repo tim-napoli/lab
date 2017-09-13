80f50bb81bf2f9d38fc93a36276631c9d0ac0698
tim <timothee.napoli@laposte.net>
2017-09-13 23:24:33 +0200

    tools/lab-data-editor: click callback on texture miniatures.
    We allows to add a callback when clicking on texture miniatures of
    the textures grid. This will be useful when reusing the textures grid in
    other situations (soon).

--- tools/qtui/textures_edit_pane.py
+++ tools/qtui/textures_edit_pane.py
@@ -1,13 +1,16 @@
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QGridLayout, QGroupBox
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QEvent, pyqtSignal
 
 from qtui import pixmap_label
 
 class texture_miniature(QWidget):
+    clicked = pyqtSignal(object)
+
     def __init__(self, parent, texture_name, texture_path):
         super().__init__(parent)
 
+        self.texture = texture_name
         self.image = pixmap_label.pixmap_label(self, QPixmap(texture_path))
 
         self.label = QLabel(texture_name, self)
@@ -18,8 +21,13 @@ class texture_miniature(QWidget):
         layout.addWidget(self.label)
         self.setLayout(layout)
 
+    def event(self, evt):
+        if evt.type() == QEvent.MouseButtonPress:
+            self.clicked.emit(self)
+        return super().event(evt)
+
 class textures_edit_pane(QWidget):
-    def __init__(self, parent, textures):
+    def __init__(self, parent, textures, on_miniature_click_cb = None):
         super().__init__(parent)
 
         self.miniatures = [
@@ -31,6 +39,8 @@ class textures_edit_pane(QWidget):
         current_column = 0
         current_row = 0
         for miniature in self.miniatures:
+            if on_miniature_click_cb != None:
+                miniature.clicked.connect(on_miniature_click_cb)
             grid.addWidget(miniature, current_row, current_column)
             current_column = current_column + 1
             if current_column == 5:
