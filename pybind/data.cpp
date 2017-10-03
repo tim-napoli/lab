488928a2d882aac0228d7161763dc517955c0a63
tim <timothee.napoli@laposte.net>
2017-10-03 16:05:08 +0200

    pybind: add the lab::data binding.

---
+++ pybind/data.cpp
@@ -0,0 +1,22 @@
+#include <pybind11/pybind11.h>
+#include "glm-cast.hpp"
+#include "lab/data/manifest.hpp"
+
+using namespace lab;
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(labdata, m) {
+    m.doc() = "lab::data python binding.";
+
+    py::class_<data::manifest>(m, "manifest")
+        .def(py::init<>())
+        .def("get_texture",     &data::manifest::get_texture)
+        .def("get_image",       &data::manifest::get_image)
+        .def("get_image_ptr",   &data::manifest::get_image_ptr)
+        .def("get_animation",   &data::manifest::get_animation)
+        .def("destroy",         &data::manifest::destroy)
+        ;
+    m.def("manifest_load",  &data::manifest::load);
+}
+
